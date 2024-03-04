becfc47fdefdbd7d1a4fab2fd459b9cd58cdf243
Michal Mrena <mrena.michal@gmail.com>
2024-03-04 09:11:27 +0100

    Init commit

---
+++ Obce-02/src/main/java/org/edupage/ssosza/Main.java
@@ -0,0 +1,7 @@
+package org.edupage.ssosza;
+
+public class Main {
+    public static void main(String[] args) {
+        DatabazaObci databazaObci = new DatabazaObci();
+    }
+}
---
+++ Obce-02/src/main/java/org/edupage/ssosza/Obec.java
@@ -0,0 +1,28 @@
+package org.edupage.ssosza;
+
+import java.util.Collections;
+import java.util.List;
+
+public class Obec {
+    private final String okres;
+    private final String nazov;
+    private final List<Integer> populacia;
+
+    public Obec(String okres, String nazov, List<Integer> populacia) {
+        this.okres = okres;
+        this.nazov = nazov;
+        this.populacia = populacia;
+    }
+
+    public String getOkres() {
+        return this.okres;
+    }
+
+    public String getNazov() {
+        return this.nazov;
+    }
+
+    public List<Integer> getPopulacia() {
+        return Collections.unmodifiableList(this.populacia);
+    }
+}
